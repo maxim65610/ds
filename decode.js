8fe434c41d56333489113231a10b4f6fd7cdd505
maxim65610 <145910061+maxim65610@users.noreply.github.com>
2023-10-01 13:03:36 +0500

    decode.js

--- decode.js
+++ decode.js
@@ -1,31 +1,19 @@
-let fs = require('fs')
-
-fs.readFile(process.argv[2], (error, inText) => {
-    if (error) {
-        console.error(error);
-        return;
+let fs = require('fs');
+let i = 0;
+let resault = '';
+let inText = fs.readFileSync('code.txt');
+inText = inText.toString();
+for (i; i < inText.length;) {
+    let ch = inText[i];
+    if (ch == '#') {
+        resault += inText[i + 2].repeat(inText[i + 1].charCodeAt(0))
+        i += 3
     }
-
-    inText = fs.readFileSync("test.txt");
-
-    inText = inText.toString();
-
-    result = ''
-    for (i = 0; i < inText.length;) {
-        let ch = inText[i]
-        if (ch == '#') {
-            result += inText[i + 2].repeat(inText[i + 1].charCodeAt(0))
-            i += 3
-        }
-        else {
-            result += ch
-            i += 1
-        }
+    else {
+        resault += ch
+        i += 1
+    }   
+    
     }
-
-    fs.writeFile(process.argv[3], result, (error) => {
-        if (error) {
-            console.error(error)
-        }
-    })
-})
+console.log(resault);
+fs.writeFileSync("test.txt", resault);
